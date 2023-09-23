64722092c762fadf79ce1662ead3c19b6303afae
Irfan Ashraf <realirfanashraf@gmail.com>
2023-09-23 12:11:53 +0530

    styles and all

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-
+import './App.scss';
+import Header from './Components/Header';
+import HomeBanner from './Components/HomeBanner';
+import Login from './Components/Login';
+import Banner from './Components/Banner';
+import List from './Components/List';
 function App() {
   return (
-    <div className="App">
-     Hello
-    </div>
+    <>
+   <Header/>
+<Banner/>
+<List/>
+    </>
   );
 }
 
---
+++ src/Components/List.js
@@ -0,0 +1,18 @@
+const List = ()=>{
+    return(
+        <div className="list">
+      <div className="row">
+        <h2 className="text-white title">d</h2>
+        <div className="col">
+          <div className="row__posters">
+            {
+              
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default List;
